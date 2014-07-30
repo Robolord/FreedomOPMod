3f7c6087f58d8cea45885482c5f0fed8708f3c56
John J. Miller <a784b2a1@opayq.com>
2014-07-29 23:48:41 -0400

    Update Command_cleanup.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_cleanup.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_cleanup.java
@@ -13,22 +13,26 @@ public class Command_cleanup extends TFM_Command
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        TFM_Util.bcastMsg(ChatColor.RED + "Atempting to start midnight cleanup");
+        TFM_Util.bcastMsg(ChatColor.RED + "Starting nightly cleanup - expect lag!");
         if (senderIsConsole)
         {
             server.dispatchCommand(sender, "tfipbanlist purge");
-            server.dispatchCommand(sender, "ops purge");
-            server.dispatchCommand(sender, "opall");
-            server.dispatchCommand(sender, "creative -a");
-            server.dispatchCommand(sender, "purgeall");
             server.dispatchCommand(sender, "tfbanlist purge");
+            server.dispatchCommand(sender, "ops purge");
+            server.dispatchCommand(sender, "ro 119");
+            server.dispatchCommand(sender, "ro 52");
+            server.dispatchCommand(sender, "rd");
+            server.dispatchCommand(sender, "opall -c");
+            server.dispatchCommand(sender, "saconfig clean");
+            server.dispatchCommand(sender, "purgeall");
             server.dispatchCommand(sender, "wipeuserdata");
-            TFM_Util.bcastMsg(ChatColor.GREEN + "Midnightly Clean Up Completed. Reloading Server");
+            server.dispatchCommand(sender, "cc");
+            TFM_Util.bcastMsg(ChatColor.GREEN + "Nighly clean up Completed. Reloading the server.");
             server.dispatchCommand(sender, "fopm reload");
         }
         else
         {
-            TFM_Util.bcastMsg(ChatColor.RED + "Cleanup failed");
+            TFM_Util.bcastMsg(ChatColor.RED + "Nightly Cleanup failed, please report this error at http://to.fop.us.to/devsupport");
         }
 
         return true;
