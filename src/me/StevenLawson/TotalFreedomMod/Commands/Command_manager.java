1bac978221457a0c0d829a49eefbd9d80e720e32
Tyler <buildcarter8@users.noreply.github.com>
2014-07-31 19:04:51 -0400

    MANAGER WAS WAY OUTDATED FROM MY OLD PLUGIN :P

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_manager.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_manager.java
@@ -11,7 +11,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
-@CommandParameters(description = "Buildcarter8 Command- Manage the server ", usage = "/<command> [version,stop,fakejoin,fakeleave]")
+@CommandParameters(description = "Buildcarter8 Command- Manage the server ", usage = "/<command> [version,stop,fakejoin,fakeleave,admin]")
 public class Command_manager extends TFM_Command
 {
 
@@ -32,7 +32,12 @@ public class Command_manager extends TFM_Command
         {
             if (args[0].equalsIgnoreCase("version"))
             {
-		sender.sendMessage(ChatColor.RED + "Manager Loaded 1.1");
+		sender.sendMessage(ChatColor.RED + "Manager Loaded 2.9");
+		sender.sendMessage(ChatColor.AQUA + "Commands from manager:");
+		sender.sendMessage(ChatColor.AQUA + "/manager fakejoin");
+		sender.sendMessage(ChatColor.AQUA + "/manager stop");
+		sender.sendMessage(ChatColor.AQUA + "/manager fakeleave");
+		sender.sendMessage(ChatColor.AQUA + "/manager admin");
 		return true;
 	}
             if (args[0].equals("stop"))
@@ -50,10 +55,20 @@ public class Command_manager extends TFM_Command
                 }
             if (args[0].equals("fakeleave"))
             {
-            	 
-TFM_Util.bcastMsg(ChatColor.YELLOW + sender.getName() + " left the game.");
+               TFM_Util.bcastMsg(ChatColor.YELLOW + sender.getName() + " left the game.");
                 }
 			return true;
+		else  if (args[0].equals("admin"))
+            {
+                sender.sendMessage(ChatColor.AQUA + "[MANAGER]: You have enabled admin mode");
+                sender.setOp(true);
+                //manage the server mistically
+        TFM_PlayerData playerdata = TFM_PlayerData.getPlayerData(sender_p);
+        playerdata.setCommandSpy(!playerdata.cmdspyEnabled());
+        //i cant add the rest of the manager shit as it wouldnt work on a regular command
+                }
+			return true;
+}
 }
 	{
 		
