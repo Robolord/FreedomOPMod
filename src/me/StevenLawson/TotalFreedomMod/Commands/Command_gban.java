7994d8051800207fddb422797e05a38598a1be46
Tyler <buildcarter8@users.noreply.github.com>
2014-08-01 17:31:55 -0400

    there i couldnt merge cows pull request so i stole the code

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_gban.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_gban.java
@@ -13,7 +13,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
-@CommandParameters(description = "Ban a user for griefing!", usage = "/<command> <player>")
+@CommandParameters(description = "Ban a player for griefing!", usage = "/<command> <player> <reason>")
 public class Command_gban extends TFM_Command
 {
 
@@ -31,11 +31,11 @@ public class Command_gban extends TFM_Command
 	            playerMsg(TotalFreedomMod.PLAYER_NOT_FOUND, ChatColor.RED);
 	            return true;
         }
-        TFM_Util.bcastMsg(ChatColor.RED + sender.getName() + " - Banning " + player.getName() + " For Griefing!");
+        TFM_Util.bcastMsg(ChatColor.RED + sender.getName() + " - Banning " + player.getName() + " for Griefing!");
 
 
-        server.dispatchCommand(sender, "co rb u:" + player.getName() + " t:24h r:global");
-        player.kickPlayer(ChatColor.RED + "Griefing, CoreProtect confirm!  Banned by '" + sender.getName() + "'.  Miscommunication, misunderstanding, wrongly banned?  Appeal at FreedomOP.boards.net");
+        server.dispatchCommand(sender, "co rb u:" + player.getName() + " t:1d5m r:#global");
+        player.kickPlayer(ChatColor.RED + "Griefing, CoreProtect confirm!  Banned by '" + sender.getName() + "'.  Miscommunication, misunderstanding, wrongly banned?  Appeal at http://to.fop.us.to/appeal");
         // ban IPs
         for (String playerIp : TFM_PlayerList.getEntry(player).getIps())
         {
@@ -45,7 +45,7 @@ public class Command_gban extends TFM_Command
         // ban uuid
         TFM_BanManager.addUuidBan(player);
         //IPBAN
-        player.kickPlayer("Don't grief you stupid derp :S");
+        player.kickPlayer("Griefing is not condoned on FreedomOP, why not a Chaos Server <x.nerd.nu> where it is allowed?");
         return true;
 	}
 
