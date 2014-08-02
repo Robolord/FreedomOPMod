4c8a058b8ed6ae7449d5ffb9e37c6e8bc5e25506
Robinson Gallego <robinson.leal7@gmail.com>
2014-08-02 15:31:57 -0400

    Fix.

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_p.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_p.java
@@ -1,5 +1,6 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
+import me.StevenLawson.TotalFreedomMod.TFM_AdminList;
 import me.StevenLawson.TotalFreedomMod.TFM_PlayerData;
 import me.StevenLawson.TotalFreedomMod.TFM_Util;
 import net.minecraft.util.org.apache.commons.lang3.StringUtils;
@@ -31,6 +32,7 @@ public class Command_p extends TFM_Command
         }
         else
         {
+            if (TFM_AdminList.isSeniorAdmin(sender))
             TFM_Util.senioradminChatMessage(sender, StringUtils.join(args, " "), senderIsConsole);
         }
 
