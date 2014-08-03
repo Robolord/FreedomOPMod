a28f2aa51d0a5ceac8d50b1d19084ee0172b0a0b
John J. Miller <a784b2a1@opayq.com>
2014-08-02 21:16:47 -0400

    Create Command_silentsetl.java

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_silentsetl.java
@@ -0,0 +1,25 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+import me.StevenLawson.TotalFreedomMod.Bridge.TFM_WorldEditBridge;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.CONSOLE)
+@CommandParameters(description = "Sets everyone's Worldedit block modification limit to 500.", usage = "/<command>")
+public class Command_silentsetl extends TFM_Command
+{
+    @Override
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+ //       TFM_Util.adminAction(sender.getName(), "Setting everyone's Worldedit block modification limit to 500.", true);
+        TFM_WorldEditBridge web = TFM_WorldEditBridge.getInstance();
+        for (final Player player : server.getOnlinePlayers())
+        {
+            web.setLimit(player, 500);
+        }
+        return true;
+    }
+}
+
+// This command will be ran inside to console, and the broadcasting has been nullified to pervent server spam.
